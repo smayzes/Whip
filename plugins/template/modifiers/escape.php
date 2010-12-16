fbeb82d94b43e20e93af6e2128e77618658f91e8
codefocus <menno@vanens.com>
2010-12-16 18:30:38 -0500

    Escape modifier

---
+++ plugins/template/modifiers/escape.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * TemplateModifierEscape class.
+ *
+ * Escape a string
+ * 
+ */
+class TemplateModifierEscape extends TemplateModifier {
+    
+    
+    public static function js($value) {
+        return addslashes($value);
+    }   //  function js
+    
+    public static function html($value) {
+        return htmlentities($value);
+    }   //  function html
+    
+	public static function run($value) {
+	   return addslashes($value);
+	}  //  function run
+    
+    
+}   //  Escape
