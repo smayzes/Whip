833dddd89192c011aef868bfac3bfdeecac2144c
codefocus <menno@vanens.com>
2011-11-12 18:21:27 -0500

    Rudimentary Memcache plugin working

--- plugins/memcache/memcache.php
+++ plugins/memcache/memcache.php
@@ -12,6 +12,8 @@
 /**
  * Memcache plugin.
  *
+ * Very rudimentary wrapper around Memcached
+ * @TODO: Working, but needs lots of work.
  *
  * Whip : A non-restrictive PHP framework
  * Copyright (c) 2010 Menno van Ens (codefocus.ca) and Shawn Mayzes (mayzes.org)
@@ -20,7 +22,136 @@
  * @extends WhipPlugin
  */
 class Memcache extends SingletonWhipPlugin {
-
-//	@TODO    
+	
+	const DEFAULT_HOST		= 'localhost';
+	const DEFAULT_PORT		= 11211;
+	
+	private $_memcache		= null;
+	
+	
+	/**
+	 * _get_memcache_instance function.
+	 * 
+	 * @access private
+	 * @return Memcached
+	 */
+	private function _get_memcache_instance() {
+		if ($this->_memcache instanceof Memcached) {
+		//	Return existing Memcached object
+			return $this->_memcache;
+		}
+	//	Create new Memcached instance
+		if (isset($this->_config['id']))
+		$this->_memcache = new Memcached($this->_config['id']);
+		
+		$servers = array();
+		if (
+			isset($this->_config['servers']) AND
+			is_array($this->_config['servers']) AND
+			0 < count($this->_config['servers'])
+		) {
+		//	Add each server to our array
+		//	and default to port 11211
+			foreach($this->_config['servers'] as $server) {
+				if (is_array($server)) {
+					if (
+						isset($server['host']) AND
+						isset($server['port']) AND
+						is_numeric($server['port'])
+					) {
+					//	We have an array of host and port
+						$servers[] = array(
+							'host' => $server['host'],
+							'port' => $server['port'],
+						);
+					}
+					elseif (isset($server['host'])) {
+					//	We have an array but only the host is set.
+						$servers[] = array(
+							'host' => $server['host'],
+							'port' => self::DEFAULT_PORT,
+						);
+					}
+				}
+				elseif (is_string($server)) {
+				//	We have a string.
+				//	Treat it as the host
+					$servers[] = array(
+						'host' => $server,
+						'port' => self::DEFAULT_PORT,
+					);
+				}
+			}	//	each configured server
+		}	//	if config array
+		
+		if (0 == count($servers)) {
+		//	No servers configured; use default server
+			$servers[] = array(
+				'host' => self::DEFAULT_HOST,
+				'port' => self::DEFAULT_PORT,
+			);
+		}	//	if no servers
+		
+	//	Add server(s) to Memcache
+		foreach ($servers as $server) {
+			$this->_memcache->addServer($server['host'], $server['port']);
+		}	//	each server
+		
+		return $this->_memcache;
+		
+	}	//	function _get_memcache_instance
+	
+	
+	/**
+	 * stats function.
+	 * Return Memcached stats.
+	 * 
+	 * @access public
+	 * @return array
+	 */
+	public function stats() {
+		$memcache = $this->_get_memcache_instance();
+		return $memcache->getStats();
+	}	//	function stats
+	
+	
+	/**
+	 * get function.
+	 * Retrieves a value from Memcached.
+	 * 
+	 * @access public
+	 * @param string $key
+	 * @return string
+	 */
+	public function get($key) {
+		$memcache = $this->_get_memcache_instance();
+		return $memcache->get($key);
+	}	//	function get
+	
+	
+	/**
+	 * set function.
+	 * Stores a value in Memcached.
+	 * 
+	 * @access public
+	 * @param string $key
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	public function set($key, $value, $expiration=null) {
+		$memcache = $this->_get_memcache_instance();
+		
+		if (null === $expiration) {
+			if (isset($this->_config['expiration'])) {
+				$expiration = $this->_config['expiration'];
+			}
+		}
+		if (!is_numeric($expiration)) {
+			$expiration = 0;
+		}
+		return $memcache->set($key, $value, $expiration);
+	}	//	function set
+	    
     
 }   //  class Memcache
+
